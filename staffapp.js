247daf898eb52cff6648cce51a11fa074eb76491
Glitch (sfs-website) <none>
2021-08-10 17:42:07 +0000

    🚨🖼 Checkpoint ./staffapp.html:9276484/8671 ./index.html:9276484/7683 ./index.css:9276484/689 ./staffapp.js:9276484/293 ./staffapp.css:9276484/2429

--- staffapp.js
+++ staffapp.js
@@ -1,3 +1,5 @@
 function show() {
-  alert("Sorry! We will announce in our announcements channel when we believe it's time to open the staff applications!");
+  alert(
+    "Sorry! We will announce in our announcements channel when we believe it's time to open the staff applications!"
+  );
 }
